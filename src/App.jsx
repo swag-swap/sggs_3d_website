d06502cce6732592c400701571f521982e0a4706
swag-swap <deshmukhswapnil316@gmail.com>
2023-07-30 14:30:44 +0530

    Added a loading page and now starting working on camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
-import { Canvas, useThree } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
-import { useEffect } from "react";
-import { CameraControls } from "@react-three/drei";
+import { Suspense, useState } from "react";
+import { Loader, Stats } from "@react-three/drei";
 
- 
+
 
 
 function App() {
+  const [start, setStart] = useState(false);
 
-  return ( 
+
+  return (
     <>
-      <Canvas shadows camera={{ position: [7.3901768904158045 , 2.9379620199168963 , 14.311720504826889], fov: 30 }}>
-        {/* <CameraControls /> */} 
-        <Experience />
+      <Canvas shadows camera={{ position: [7.3901768904158045, 2.9379620199168963, 14.311720504826889], fov: 20 }}>
+        <Suspense fallback={null}>
+          <Experience />
+        </Suspense> 
+
       </Canvas>
+      <Loader
+        preload 
+        dataInterpolation={(p) => `Welcome to SGGS! ${p.toFixed(2)}%`}
+      />
     </>
   );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,8 +9,7 @@ export const Experience = () => {
   return (
     <>
       <MapControls />
-      <Stars speed={5} saturation={10}/>  
-      
+      <Stars speed={5} saturation={10}/>   
       <color attach="background" args={[0x000000]} />
       <fog attach="fog" args={[0x000000, 10, 35]} />
       <ambientLight intensity={0.1} />
